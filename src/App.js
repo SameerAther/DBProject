becf9ba50db05d8726ea8ea17031cda445d8060d
Noor Ali <nooralib180@gmail.com>
2022-07-10 00:48:27 +0500

    logout functionality added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React , { Component } from 'react';
-import {Routes, Route, Navigate } from 'react-router-dom';
+import {Routes, Route } from 'react-router-dom';
 
 import { Header } from './components/Header.component.jsx';
 import { SignUp } from './pages/SignUp.jsx';
